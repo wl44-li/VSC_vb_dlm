a69c32d78b2966981c4817519f76e8b9171fcf6f
wl44-mba <weiye_li@yahoo.com>
2023-11-17 16:23:17 +0000

    added M-I projection plot for KL

--- thesis_figures.jl
+++ thesis_figures.jl
@@ -35,7 +35,7 @@ function gibbs_plot()
     display(p)
 end
 
-gibbs_plot()
+#gibbs_plot()
 
 function i_projection_plot()
     # Assume a target GMM with 3 components
@@ -46,27 +46,29 @@ function i_projection_plot()
     # Generate data from the target GMM
     n_samples = 100
     data = []
-    for i in 1:n_samples
+    for _ in 1:n_samples
         z = rand(Categorical(weights))
         x = rand(Normal(means[z], stds[z]))
         push!(data, x)
     end
 
     # Assume the I-projection leads to a single Gaussian with mean 2.0 and std 1.0
-    iproj_mean = 4.8
-    iproj_std = 1.0
+    iproj_mean, m_proj_mean = 4.99, -1.0
+    iproj_std,  m_proj_std  = 1.11, 5.5
 
     # Plot the target GMM and I-projection
     x = range(-10, stop=10, length=200)
     y_true = sum(w * pdf.(Normal(μ, σ), x) for (w, μ, σ) in zip(weights, means, stds))
     y_iproj = pdf.(Normal(iproj_mean, iproj_std), x)
 
+    y_mproj = pdf.(Normal(m_proj_mean, m_proj_std), x)
     # Scale down y_true to match the maximum value of y_iproj
     scale_factor = maximum(y_iproj) / maximum(y_true)
-    y_true_scaled = y_true * scale_factor
-
-    p = plot(x, y_true_scaled, label="Target PDF", linewidth=2)
+    y_true_scaled = y_true * scale_factor * 0.9
+    y_mproj = y_mproj * 1.5
+    p = plot(x, y_true_scaled, label="Target p(x)", linewidth=2, xlabel="x", ylabel="q(x)")
     plot!(x, y_iproj, label="I-Projection", linewidth=2, linestyle=:dash, color=:red)
+    plot!(x, y_mproj, label="M-Projection", linewidth=2, linestyle=:dash, color=:orange)
     display(p)
 end
 
